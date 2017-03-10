bfe3ed79284d0be2db5e1865a2a090c89b796558
George Komarov, MIPT <geoolekom@gmail.com>
2017-03-10 15:35:17 +0300

    Improved iterators perfomance

--- src/utils/processors.py
+++ src/utils/processors.py
@@ -1,8 +1,10 @@
+import itertools
+
+
 def data_to_hist(handler_list, data):
 	stats_list = [dict() for _ in handler_list]
 
-	for handler, stats in zip(handler_list, stats_list):
-		[handler(stats, elem) for elem in data]
+	[handler(stats, elem) for (handler, stats), elem in itertools.product(zip(handler_list, stats_list), data)]
 
 	# map(
 	# 	lambda handler, stats: map(lambda elem: handler(stats, elem), data),
